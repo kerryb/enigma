e006fe1574b5e9fa7d90594e97f1fc965f11248d
Kerry Buckley <kerryjbuckley@gmail.com>
2015-04-02 22:51:08 +0100

    Encrypt through rotors and back

--- lib/machine.rb
+++ lib/machine.rb
@@ -1,10 +1,17 @@
 class Machine
-  def initialize rotors:
+  def initialize rotors:, reflector:
     @rotors = rotors
+    @reflector = reflector
   end
 
-  def type letter
+  def encrypt letter
     rotors_to_advance.each(&:advance)
+    letters = (?A..?Z).to_a
+    input_position = letters.index(letter)
+    reflector_input = @rotors.reduce(input_position) {|position, rotor| rotor.translate_left position }
+    reflector_output = @reflector.map reflector_input
+    output_position = @rotors.reverse.reduce(reflector_output) {|position, rotor| rotor.translate_right position }
+    letters[output_position]
   end
 
   private
--- spec/machine_spec.rb
+++ spec/machine_spec.rb
@@ -4,36 +4,48 @@ describe Machine do
   let(:left_rotor) { spy :left_rotor, rollover?: false }
   let(:middle_rotor) { spy :middle_rotor, rollover?: false }
   let(:right_rotor) { spy :right_rotor, rollover?: false }
-  subject { described_class.new rotors: [right_rotor, middle_rotor, left_rotor] }
+  let(:reflector) { spy :reflector }
+  subject { described_class.new rotors: [right_rotor, middle_rotor, left_rotor], reflector: reflector }
 
-  describe "#type" do
+  describe "#encrypt" do
     it "advances the first rotor" do
-      subject.type "A"
+      subject.encrypt "A"
       expect(right_rotor).to have_received :advance
     end
 
     it "advances other rotors if adjacent to a rollover notch" do
       allow(right_rotor).to receive(:rollover?) { true }
-      subject.type "A"
+      subject.encrypt "A"
       expect(middle_rotor).to have_received :advance
     end
 
     it "does not advance other rotors if not adjacent to a rollover notch" do
       allow(right_rotor).to receive(:rollover?) { false }
-      subject.type "A"
+      subject.encrypt "A"
       expect(middle_rotor).not_to have_received :advance
     end
 
     it "advances the notched rotor along with the one to its left" do
       allow(middle_rotor).to receive(:rollover?) { true }
-      subject.type "A"
+      subject.encrypt "A"
       expect(middle_rotor).to have_received :advance
     end
 
     it "only advances the right rotor once, even when rolling over the middle one" do
       allow(right_rotor).to receive(:rollover?) { true }
-      subject.type "A"
+      subject.encrypt "A"
       expect(right_rotor).to have_received(:advance).once
+    end
+
+    it "sends the signal through the rotors to the reflector and back" do
+      allow(right_rotor).to receive(:translate_left).with(0) { 1 }
+      allow(middle_rotor).to receive(:translate_left).with(1) { 2 }
+      allow(left_rotor).to receive(:translate_left).with(2) { 3 }
+      allow(reflector).to receive(:map).with(3) { 4 }
+      allow(left_rotor).to receive(:translate_right).with(4) { 5 }
+      allow(middle_rotor).to receive(:translate_right).with(5) { 6 }
+      allow(right_rotor).to receive(:translate_right).with(6) { 7 }
+      expect(subject.encrypt "A").to eq "H"
     end
   end
 end
